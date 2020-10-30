907c199ceb8f2b356d9eeb78eb115adf23f0f15a
millerbee <millerbgos@gmail.com>
2020-10-30 09:19:37 -0600

    code clean up

--- employee.sql
+++ employee.sql
@@ -1,30 +1,48 @@
---this is the view I created in mysql to format and select from joined tables.
+--DROP DATABASE IF EXISTS employee;
+--CREATE DATABASE employee;
 
-create view emp_data AS
- SELECT distinct
-        CONCAT(`e`.`first_name`, ' ', `e`.`last_name`) AS `Employee`,
-        `r`.`title` AS `Role`,
-        `d`.`name` AS `Dept`
-    FROM
-        (`employee`.`department` `d`
-        JOIN (`employee`.`roles` `r`
-      left  JOIN `employee`.`employees` `e` ON ((`r`.`role_id` = `e`.`role_id`))))
-    WHERE
-        (`e`.`dept_id` = `d`.`dept_id`)
-        
+USE employee;
 
+CREATE TABLE employees(
+  emp_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (emp_id)
+);
 
 
-select * from emp_data
-where role = 'Manager'
+CREATE TABLE department(
+  dept_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (dept_id)
+);
+
+CREATE TABLE roles(
+  role_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL(10,2),
+  dept_id INT,
+  PRIMARY KEY (role_id)
+);
 
 
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Sales");
 
 
+INSERT INTO employees (first_name, last_name)
+VALUES ("David", "Bowie");
 
+INSERT INTO employees (first_name, last_name)
+VALUES ("Sandra", "Smith");
 
-select sum(roles.salary), employees.role_id, roles.role_id, roles.dept_id, department.dept_id, department.name
-from employees, roles, department
-where employees.role_id =roles.role_id
-and roles.dept_id = department.dept_id
-group by department.name;
+INSERT INTO roles (title, salary)
+VALUES ("Finance Manager", "100000");
+
+INSERT INTO roles (title, salary)
+VALUES ("Sales Director", "200000");
